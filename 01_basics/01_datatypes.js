b85504cd02e66c2d31e514f68fb35f15ebee4961
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-10-11 11:04:08 +0530

    Java script

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) //we are using node, it can be executed in browser
+
+console.log(3 +
+    3)
+    // readability should be high
+
+console.log("Krishna")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number can be upto 2 to power 53
+// bigint
+// string ==> ""
+// undefined ==> no value assigned
+// null is an object and a standalone value
+//symbol ==> unique
+
+console.log(typeof "Krishna")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof null)
+console.log(typeof undefined)
+
